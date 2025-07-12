6557df422eb08aa789a19559a0362382170df5e7
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2025-07-12 16:51:59 +0900

    Add files via upload

---
+++ 오늘은 토요일 11.py
@@ -0,0 +1,66 @@
+##s = input()
+##print(len(s))
+##
+##s = input()
+##if "@" in s:
+##    prin("이메일 입니다.")
+##
+##s = input
+##print(s.find("a"))
+##
+##s = input()
+##print(s*3)
+##
+##s1 = input()
+##s2 = input()
+##print(s1 + s2)
+##
+##s = input()
+##print(s[;;-1])
+##
+##s = input()
+##print(s.count("a"))
+
+##s = input()
+##count = 0
+##for ch in s:
+##    if 'a' <= ch <= 'z':
+##        count += 1
+##print("소문자갯수:", count)
+##
+##s = input()
+##for ch in s:
+##    print(ch)
+
+##s = input()
+##print(s[0].isupper()
+
+##s = input()
+##print(s.swapcase())
+
+##s = input()
+##count = 0
+##for ch in s:
+##    if '0' <= ch <='9':
+##        count += 1
+##print(count)
+
+##s = input()
+##if s[0] == s[-1]:
+##    print("같음")
+
+##s = input()
+##print('-'*(len(s)))
+
+s = input()
+for letter in s
+
+if letter in a:
+    result += letter
+    result = ''
+print("알파벳만 출력된 문자열:", result)
+
+
+
+
+
