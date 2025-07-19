2885a61dff29bf67b7d14c5b5dd1246923f9f9d1
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2025-07-19 16:48:55 +0900

    Add files via upload

---
+++ 오늘은 원래 토요일 1.py
@@ -0,0 +1,54 @@
+##n = int(input())
+##nums = list(map(int,input().split()))
+##print(sum(nums))
+
+##
+##nums = list(map(int,input().split()))
+##print(max(nums))
+
+
+##nums = list(map(int,input().split()))
+##for n in nums:
+##    if n%2 ==0:
+
+##nums = list(map(int,input().split()))
+##cnt = 0
+##for n in nums:
+##    if n>=5:
+##        cnt +=1
+##    print(cnt)
+
+##nums = list(map(int,input().split()))
+##reversed_nums = print(nums[::-1])
+##print(*reversed_nums)
+
+##nums = list(map(int,input().split()))
+##for n in nums:
+##    if n%2 ==1:
+##        print(n)
+
+##nums = list(map(int,input().split()))
+##for i in range(len(nums)-1):
+##    print(abs(nums[i] - nums[i+1]))
+##
+##nums = list(map(int,input().split()))
+##average = sum(nums) / len(nums)
+##for num in nums:
+##    if num > average:
+##        print(num)
+
+##nums = list(map(int,input().split()))
+##print(max(nums))
+##print(min(nums))
+
+nums = list(map(int,input().split()))
+result = []
+for n in nums:
+    if n%2 == 0:
+      result.append(n)  
+print(result)
+
+
+        
+        
+
