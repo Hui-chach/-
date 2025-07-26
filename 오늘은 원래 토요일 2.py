3e033a1d047e9e031149585dc749e14139f327da
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2025-07-26 16:48:41 +0900

    Add files via upload

---
+++ 오늘은 원래 토요일 2.py
@@ -0,0 +1,54 @@
+##a = int(input())
+##b = int(input())
+##print(a+b)
+
+##
+##str1 = input("첫번째 문자열을 입력하세요: ")
+##str2 = input("두번째 문자열을 입력하세요: ")
+##
+##
+##if len(str1) > len(str2):
+##    print(str1)
+##else:
+##    print(str2)
+
+
+##N = int(input("자연수 N을 입력하세요 (1 이상 100 이하): "))
+##for i in range(N, 0, -1):
+##    print('*' * i)
+
+
+##s = input()
+##print('"' + s + '"')
+
+
+##num = int(input())
+##a = abs(num)
+##print(a)
+
+
+##a, b = map(int, input().split())
+##if a == b:
+##    print(a + b)
+##else:
+##    print(b - a)
+
+##n = int(input())
+##i = 1
+##while i <= n:   
+##    print(i)
+##    i = i + 1  
+
+N = input()
+for i in range(len(N) - 1, -1, -1):
+    print(N[i], end='')
+
+
+
+
+
+
+
+
+
+
