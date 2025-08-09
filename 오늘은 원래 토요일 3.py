ef5ed835e5fd735e41fcc625710d199f0dfc107c
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2025-08-09 16:51:47 +0900

    Add files via upload

---
+++ 오늘은 원래 토요일 3.py
@@ -0,0 +1,79 @@
+##n = int(input())
+##m = int(input())
+##for i in range(n, m+1, 5):
+##print(i)
+
+##n = int(input())
+##nums = list(map(int,input().split()))
+
+
+##N = int(input("자연수 N을 입력하세요: "))
+##for i in range(N, 101, 3):
+##    print(i)
+
+
+##
+##N = int(input("입력: "))
+##
+##for i in range(N):
+##    underscores = '_' * i
+##    star = '*' * (2 *)
+##    print(highpone + star + highpone)
+##    ]
+
+
+
+##text = input("문자열을 입력하세요: ")
+##count = 0
+##for char in text:
+##    if char.isupper():
+##        count += 1
+##if count > 0:
+##    print(count)
+##else:
+##    print(-1)
+
+
+##N = int(input())
+##arr = list(map(int, input().split()))
+##ans = 0
+##for a in arr:
+##    if a in [3, 6, 9]:
+##        ans += 1
+##print("%d"%ans)
+
+##s1 = input()
+##s2 = input()
+##s3 = input()
+##print(s3)
+##print(s2)
+##print(s1)      
+
+
+##s1 = input()
+##s2 = input()
+##if s1==s2:
+##   print("yes")
+##else:
+##    print("N0")
+
+##s = input()
+##for i in range(len(s)) :
+##    for j in range(i + 1):
+##        print(s[j], end = '')
+##    print()
+
+
+
+text = input("문자열을 입력하세요: ")
+result = ""
+for i in range(len(text)):
+    result += text[i]
+    if i != len(text) - 1:
+        result += '/'
+print(result)
+
+
+
+
+
