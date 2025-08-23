7d1a91994f674b031fe75e236ea352cb9b403c2c
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2025-08-23 16:42:05 +0900

    Add files via upload

---
+++ 오늘은 언래 토요일 4.py
@@ -0,0 +1,88 @@
+##n = int(input("자연수 n을 입력하세요: "))
+##count = 0
+##for i in range(1, n + 1):
+##    if n % i == 0:
+##        count += 1
+##print("약수의 개수:", count)
+
+##n = int(input("숫자입력: "))
+##total = 0
+##for i in range(1, n + 1):
+##    total = total + i
+##    print(n)
+
+    
+##n = int(input())
+##text = input()
+##print(text * n)
+
+##i = 1
+##n = int(input(""))
+##
+##while i <= n:
+##    print(i, i + 1)
+##    i += 2
+
+##a = input("")
+##b = input("")
+##c = input("")
+##
+##a = int(a)
+##b = int(b)
+##c = int(c)
+##
+##if a >= 60 and b >= 60 and c >= 60:
+##   print("YES")
+##else:
+##    print("NO")
+
+
+##a = 1
+##while a<100:
+##    if a%3 == 0 or a%7 == 0:
+##        print(a,end = ' ')
+##    a+=1
+
+##n = int(input())
+##m = int(input())
+##print(n%m)
+##arr = []
+##for i in range(10):
+##    num = int(input())
+##    arr.append(num)
+##for i in arr:
+##    if i % 5 == 0:
+##        print(i, end=' ')
+##count = 0
+##total = 0
+##print()  
+##if count > 0:
+##    avr = round()
+##else:
+##    avr = 0
+##print(count, total, avr)
+
+text = input()  
+char = input()    
+
+result = ""      
+
+for i in text:
+    if i != char:
+        result = result + i
+print(result)
+
+
+
+   
+
+ 
+
+
+
+
+
+
+
+
+ 
